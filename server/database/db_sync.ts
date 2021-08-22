5814eaa92817107435ff9b4900a8c14f8a53bd3a
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-22 18:37:23 +0700

    Connect to database

---
+++ server/database/db_sync.ts
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+export const initDb = () => {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    require('./sequelize_options.json'))
+
+  sequelize.sync({ force: false, alter: false })
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log('PostgreSQL connected…')
+    })
+    // eslint-disable-next-line no-console
+    .catch((err: any) => console.error(err))
+}
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express'
+import { initDb } from './database/db_sync'
+
+try {
+  require('dotenv').config()
+} catch (e) {}
+
 const express = require('express')
 const app = express()
+initDb()
 
 app.get('/echo/:what', (req: Request, res: Response) => {
   res.json(req.params)
