d5bb32d8743e39abdd0f4c6e843e80b68f3bada0
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-27 18:23:47 +0700

    Catch check database process

--- server/parsers/checkDatabase.ts
+++ server/parsers/checkDatabase.ts
@@ -3,15 +3,21 @@ import { EditionsNews, NewsToCheck } from './support/types'
 
 export const checkDatabase = async (newsToCheck: NewsToCheck[]) => {
   const filters = await Promise.all(newsToCheck.map(async (news) => {
-    const id = await models.News.findOne({
-      where: {
-        publicationDate: news.publicationDate,
-        titleUrl: news.titleUrl,
-        editionId: news.editionId
-      },
-      attributes: ['editionId']
-    })
-    return !id
+    try {
+      const id = await models.News.findOne({
+        where: {
+          publicationDate: news.publicationDate,
+          titleUrl: news.titleUrl,
+          editionId: news.editionId
+        },
+        attributes: ['editionId']
+      })
+      return !id
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error('Failed to check news: ', news)
+      return false
+    }
   }))
 
   return newsToCheck.filter((_v, index) => filters[index])
