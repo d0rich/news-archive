7ae392b64f9bcebb593fe12d18271dfe821a1708
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-27 18:28:09 +0700

    Nightmare -> puppeteer

--- server/parsers/getPageHTML.ts
+++ server/parsers/getPageHTML.ts
@@ -1,17 +1,15 @@
-import Nightmare from 'nightmare'
+import puppeteer from 'puppeteer'
 import axios from 'axios'
 
 export const getDynamicPageHTML = async (url: string) => {
-  console.log('Starting browser to load ' + url)
-  const nightmare = new Nightmare({ show: false })
-  console.log('Browser started. Getting ' + url + ' ...')
+  console.log('Starting browser to get ' + url + '...')
+  const browser = await puppeteer.launch()
+  console.log('Getting ' + url + ' ...')
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.waitForTimeout(1000)
   // @ts-ignore
-  const html: string = await (nightmare
-    .goto(url)
-    .wait('body')
-    // @ts-ignore
-    .evaluate(() => document.querySelector('body').innerHTML)
-    .end())
+  const html: string = await page.evaluate(() => document.querySelector('body').innerHTML)
   console.log(`Page ${url} got`)
   return html
 }
