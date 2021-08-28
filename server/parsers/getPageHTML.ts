dce6cf87a9a99e2673a26aad41307786f277476b
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-28 12:20:19 +0700

    Close puppeteer browser

--- server/parsers/getPageHTML.ts
+++ server/parsers/getPageHTML.ts
@@ -1,20 +1,20 @@
 import puppeteer from 'puppeteer'
 import axios from 'axios'
 
-const browser = await puppeteer.launch({
-  args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox'
-  ]
-})
-
 export const getDynamicPageHTML = async (url: string) => {
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  })
   const page = await browser.newPage()
   await page.goto(url)
   await page.waitForTimeout(1000)
   // @ts-ignore
   const html: string = await page.evaluate(() => document.querySelector('body').innerHTML)
   await page.close()
+  await browser.close()
   return html
 }
 
