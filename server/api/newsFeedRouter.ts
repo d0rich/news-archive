629377007c01592d5201ee59f754a466802884e6
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-29 13:27:34 +0700

    API GET request for monthly news

---
+++ server/api/newsFeedRouter.ts
@@ -0,0 +1,52 @@
+import { Router, Request, Response } from 'express'
+import { Op, literal } from 'sequelize'
+import { models } from '../server'
+import { to2Letters } from '../parsers/support/functions'
+
+const router = Router()
+
+router.get('/year/:year/month/:month', async (req: Request, res: Response) => {
+  try {
+    // getting params
+    const year = +req.params.year
+    const month = +req.params.month
+    const daysInMonth = 33 - new Date(year, month - 1, 33).getDate()
+    // getting 5 random news for each day in month
+    const dailyNewsPromises: Promise<any[]>[] = []
+    for (let day = 1; day <= daysInMonth; day++) {
+      const date = `${year}-${to2Letters(month)}-${to2Letters(day)}`
+      const dailyNewsPromise = models.News.findAll({
+        where: { publicationDate: { [Op.eq]: date } },
+        attributes: ['publicationDate', 'title', 'description', 'editionId'],
+        limit: 5,
+        order: literal('random()')
+      })
+      dailyNewsPromises.push(dailyNewsPromise)
+    }
+    const news = (await Promise.all(dailyNewsPromises))
+      .filter(dailyNews => dailyNews.length > 0)
+      .map((dailyNews) => {
+        return {
+          date: dailyNews[0].dataValues.publicationDate,
+          news: dailyNews
+        }
+      })
+    // counting all news in month
+    const newsCount = await models.News.count({
+      where: {
+        publicationDate: {
+          [Op.gte]: `${year}-${to2Letters(month)}-01`,
+          [Op.lte]: `${year}-${to2Letters(month)}-${to2Letters(daysInMonth)}`
+        }
+      }
+    })
+    res.send({ _found: newsCount, news })
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e)
+    res.statusCode = 500
+    res.send(e)
+  }
+})
+
+export const newsFeedRouter = router
--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express'
 import { scheduleJob } from 'node-schedule'
 import { initDb } from './database/db_sync'
 import { parseAll } from './parsers'
+import { newsFeedRouter } from './api/newsFeedRouter'
+
 try {
   require('dotenv').config()
 } catch (e) {}
@@ -17,6 +19,7 @@ scheduleJob('/15 * * * *', parseAll)
 app.get('/echo/:what', (req: Request, res: Response) => {
   res.json(req.params)
 })
+app.use('/news', newsFeedRouter)
 
 module.exports = {
   path: '/api',
