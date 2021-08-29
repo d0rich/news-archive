910c764d7c44ca4642618b5f6c16c2112d8ab1a2
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-29 22:20:45 +0700

    Base url in env

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:3000/api'
+    baseURL: process.env.AXIOS_BASE_URL
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
