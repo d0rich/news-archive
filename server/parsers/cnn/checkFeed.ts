fc5c39e7bb67e933a061a2855f030a49b8f13a2b
Nikolay Dorofeev <d.orich@yandex.com>
2021-12-23 14:59:06 +0700

    Catch errors on parse CNN news

--- server/parsers/cnn/checkFeed.ts
+++ server/parsers/cnn/checkFeed.ts
@@ -3,31 +3,36 @@ import { NewsToCheck } from '../support/types'
 import { getDynamicPageHTML } from '../getPageHTML'
 
 export const checkCnnFeed = async () => {
-  const feedPage = await getDynamicPageHTML('https://edition.cnn.com/')
-  const $ = cheerio.load(feedPage)
-  let newsNodes: NodeWithChildren[] = []
-  const parsedFeed = $('h3.cd__headline',
-    '.zn__containers > .column > ul.cn > li > article > .cd__wrapper > .cd__content')
-  for (const index in parsedFeed) {
-    if (!isNaN(+index)) { newsNodes.push(parsedFeed[index]) }
-  }
-  newsNodes = newsNodes.filter((node) => {
-    const href = $('a', node).attr('href')
-    // @ts-ignore
-    return !isNaN(+href?.split('/')[1]) && !isNaN(+href?.split('/')[2]) && href?.split('/').length === 7
-  })
-  const news: NewsToCheck[] = newsNodes.map((node) => {
-    const href = $('a', node).attr('href')
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_s, year, month, day, type, titleUrl, _index] = href?.split('/')
-    return {
-      publicationDate: new Date(`${year}-${month}-${day}`),
-      titleUrl,
-      editionId: 2,
-      locale: 'en',
-      url: href?.slice(1)
+  try {
+    const feedPage = await getDynamicPageHTML('https://edition.cnn.com/')
+    const $ = cheerio.load(feedPage)
+    let newsNodes: NodeWithChildren[] = []
+    const parsedFeed = $('h3.cd__headline',
+      '.zn__containers > .column > ul.cn > li > article > .cd__wrapper > .cd__content')
+    for (const index in parsedFeed) {
+      if (!isNaN(+index)) { newsNodes.push(parsedFeed[index]) }
     }
-  })
-  return news
+    newsNodes = newsNodes.filter((node) => {
+      const href = $('a', node).attr('href')
+      // @ts-ignore
+      return !isNaN(+href?.split('/')[1]) && !isNaN(+href?.split('/')[2]) && href?.split('/').length === 7
+    })
+    const news: NewsToCheck[] = newsNodes.map((node) => {
+      const href = $('a', node).attr('href')
+      // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [_s, year, month, day, type, titleUrl, _index] = href?.split('/')
+      return {
+        publicationDate: new Date(`${year}-${month}-${day}`),
+        titleUrl,
+        editionId: 2,
+        locale: 'en',
+        url: href?.slice(1)
+      }
+    })
+    return news
+  } catch (e) {
+    console.error(e)
+    return []
+  }
 }
